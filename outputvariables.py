82802a1a9223dedfc6566c5261d0243d3fba5f9b
chetanesoo <chetanesoo@gmail.com>
2025-07-11 21:35:42 +0400

    outputvariables.py

---
+++ outputvariables.py
@@ -0,0 +1,20 @@
+x = "Python is awesome"
+print(x)  # This will print "Python is awesome"
+
+a = "Python"
+b = "is"
+c = "awesome"
+print(a, b, c)  # This will print "Python is awesome"
+
+d = "Python "
+e = "is "
+f = "awesome"
+print(d + e + f)  # This will also print "Python is awesome"
+
+g = 5
+h = 10
+print(g + h)  # This will print 15
+
+i = 5
+j = "John"
+print(i, j)  # This will print "5 John"
