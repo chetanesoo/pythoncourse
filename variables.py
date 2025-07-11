5efa19cf9e85a9a359e097e648f3227666921017
chetanesoo <chetanesoo@gmail.com>
2025-07-11 21:23:18 +0400

    variables.py

---
+++ variables.py
@@ -0,0 +1,24 @@
+x = 5
+y = "John"
+print(x) # This will print 5
+print(y) # This will print "John"
+
+z = 4 #x is of type int
+z = "Sally" #x is now of type str
+print (z) # This will print "Sally" and overwrite the previous value of z
+
+a = str(3) #a will be '3'
+b = int(3) #b will be 3
+c = float(3) #c will be 3.0
+print(type(a)) # This will print <class 'str'>
+print(type(b)) # This will print <class 'int'>
+print(type(c)) # This will print <class 'float'>
+
+d = "John" 
+# d is the same and provide same value
+d = 'John'
+
+
+a = 4
+A = "Sally"
+# A is not the same as a, Python is case-sensitive when it comes to variable names
