a2a3b76332ecd6b9f21236822a82f5ba76ed659f
chetanesoo <chetanesoo@gmail.com>
2025-07-11 21:28:20 +0400

    multiplevalues.py

---
+++ multiplevalues.py
@@ -0,0 +1,15 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)  # This will print "Orange"
+print(y)  # This will print "Banana"
+print(z)  # This will print "Cherry"
+
+x = y = z = "Orange"
+print(x)  # This will print "Orange"
+print(y)  # This will print "Orange"
+print(z)  # This will print "Orange"
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)  # This will print "apple"
+print(y)  # This will print "banana"
+print(z)  # This will print "cherry"
