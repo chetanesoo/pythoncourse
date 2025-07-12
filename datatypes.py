52565d53a720f476f210bae4489c2f53908df2d2
chetanesoo <chetanesoo@gmail.com>
2025-07-12 18:44:27 +0400

    datatypes.py

--- datatypes.py
+++ datatypes.py
@@ -2,25 +2,46 @@ x = 5
 print(type(x)) # This code snippet prints the type of the variable x, which is an integer.
 
 a = "Hello World"
-print(type(a))
+print(type(a)) # This code snippet prints the type of the variable a, which is an string.
 
 b = 20
-print(type(b))
+print(type(b)) # This code snippet prints the type of the variable b, which is an integer.
 
 c = 20.5
-print(type(c))
+print(type(c)) # This code snippet prints the type of the variable c, which is an float.
 
 d = 1j
-print(type(d))
+print(type(d)) # This code snippet prints the type of the variable d, which is an complex.
 
-e = ["apple", "banana", "cherry"]
+e = ["apple", "banana", "cherry"] # This code snippet prints the type of the variable e, which is an list.
 print(type(e))
 
-f = ("apple", "banana", "cherry")
+f = ("apple", "banana", "cherry") # This code snippet prints the type of the variable f, which is an tuple.
 print(type(f))
 
-g = range(6)
+g = range(6) # This code snippet prints the type of the variable g, which is an range.
 print(type(g))
 
-h = {"name" : "john", "age" : 36}
+h = {"name" : "john", "age" : 36} # This code snippet prints the type of the variable h, which is an dictionary.
 print(type(h))
+
+i = {"apple", "banana", "cherry"} # This code snippet prints the type of the variable i, which is an set.
+print(type(i))
+
+j = frozenset({"apple", "banana", "cherry"}) # This code snippet prints the type of the variable j, which is an frozenset.
+print(type(j))
+
+k = True # This code snippet prints the type of the variable k, which is an boolean.
+print(type(k))
+
+l = b"Hello" # This code snippet prints the type of the variable l, which is an bytes.
+print(type(l))
+
+m = bytearray(5) # This code snippet prints the type of the variable m, which is an bytearray.
+print(type(m))
+
+n = memoryview(bytes(5)) # This code snippet prints the type of the variable n, which is an memoryview.
+print(type(n))
+
+o = None # This code snippet prints the type of the variable o, which is an NoneType.
+print(type(o))
