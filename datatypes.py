8188995028e6f3da3aff80af73c3b147792831d1
chetanesoo <chetanesoo@gmail.com>
2025-07-12 18:25:08 +0400

    datatypes

--- datatypes.py
+++ datatypes.py
@@ -4,5 +4,23 @@ print(type(x)) # This code snippet prints the type of the variable x, which is a
 a = "Hello World"
 print(type(a))
 
-b = "20"
+b = 20
 print(type(b))
+
+c = 20.5
+print(type(c))
+
+d = 1j
+print(type(d))
+
+e = ["apple", "banana", "cherry"]
+print(type(e))
+
+f = ("apple", "banana", "cherry")
+print(type(f))
+
+g = range(6)
+print(type(g))
+
+h = {"name" : "john", "age" : 36}
+print(type(h))
