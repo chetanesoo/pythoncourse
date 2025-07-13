2686435ebed215f637f743cd09575ca1d172a3e0
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-13 15:24:34 +0400

    modifystring.py

---
+++ modifystring.py
@@ -0,0 +1,11 @@
+a = "Hello, World!"
+print(a.upper()) #This is a method, it converts the string to uppercase
+
+b = "Hello, World!"
+print(b.lower()) #This is a method, it converts the string to lowercase
+
+c = " Hello, World! "
+print(c.strip()) #This is a method, it removes the whitespace from the beginning and end of the string
+
+d = "Hello, World!"
+print(d.replace("H", "J")) #This is a method, it replaces the character H with J
