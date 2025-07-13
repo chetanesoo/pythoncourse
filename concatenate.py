d840101a7926e327d960eb9f35f4e5f88821bdaf
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-13 15:47:21 +0400

    concatenate.py

---
+++ concatenate.py
@@ -0,0 +1,13 @@
+a = "Hello"
+b = "World"
+c = a + b
+print(c) #This is a concatenation, it joins the two strings together
+
+d = a + " " + b
+print(d) #This is a concatenation, it joins the two strings together with a space in between
+
+e = a * 3
+print(e) #This is a concatenation, it repeats the string three times
+
+f = a + " " + b * 3
+print(f) #This is a concatenation, it joins the two strings together with a space in between and repeats the string three times
