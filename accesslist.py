1db3b3611816ee6154f51d97e0648c125580d56c
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-13 21:01:11 +0400

    accesslist.py

---
+++ accesslist.py
@@ -0,0 +1,17 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1]) #prints the second item in the list
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1]) #prints the last item in the list
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5]) #prints the items from the third to the fifth item
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:2]) #prints the items from the first to the second item - will stop at the third item
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]) #prints the items from the third item to the end of the list - will start at the third item and go to the end of the list
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1]) #prints the items from the fourth to the last item - will start at the fourth item and go to the second to last item
