730f62d1e6e8b318172498720723e945e7c794f2
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-13 16:55:45 +0400

    operators.py

---
+++ operators.py
@@ -0,0 +1,23 @@
+print(10 + 7) #This is an arithmetic operator, it adds 10 and 5
+print(10 - 7) #This is an arithmetic operator, it subtracts 10 and 5
+print(10 * 7) #This is an arithmetic operator, it multiplies 10 and 5
+print(10 / 7) #This is an arithmetic operator, it divides 10 and 5
+print(10 % 7) #This is an arithmetic operator, it returns the remainder of 10 divided by 5
+print(10 ** 7) #This is an arithmetic operator, it raises 10 to the power of 5
+print(10 // 7) #This is an arithmetic operator, it returns the integer part of 10 divided by 5
+
+x = 5
+y = 3
+print(x == y) #This is a comparison operator, it checks if x is equal to y
+
+a = 5
+a += 3
+print(a) #This is an assignment operator, it adds 3 to a
+
+b = 5
+b -= 3
+print(b) #This is an assignment operator, it subtracts 3 from b
+
+c = 5
+c *= 3
+print(c) #This is an assignment operator, it multiplies 3 by c
