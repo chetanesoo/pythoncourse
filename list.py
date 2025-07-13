6f6f28c9ad20c118429df4b3d364a289035f7fc9
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-13 17:13:42 +0400

    list.py

---
+++ list.py
@@ -0,0 +1,20 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+
+thelist = ["apple", "banana", "apple", "cherry"] #This is a list with duplicate items
+print(thelist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) #This is a function, it returns the length of the list
+
+list1 = ["abc", 34, True, 40, "male"]
+print(type(list1)) #This is a function, it returns the type of the list
+
+list2 = list(("apple", "banana", "cherry")) #This is a constructor, it creates a list
+print(list2)
+
+list3= ["apple", "banana", "cherry"]
+print(list3[1]) #This is a list, it returns the second item in the list
+
+list4 = ["apple", "banana", "cherry"]
+print(list4[-1]) #This is a list, it returns the last item in the list
