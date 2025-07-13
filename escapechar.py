61922f0aec52e5b2d5c143667972237c4815b0da
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-13 15:59:33 +0400

    escapechar.py

---
+++ escapechar.py
@@ -0,0 +1,17 @@
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) #This is a string with a double quote
+
+txt1 = "We are the so-called \'Vikings\' from the north."
+print(txt1) #This is a string with a single quote
+
+txt2 = "We are the so-called \nVikings from the north."
+print(txt2) #This is a string with a new line
+
+txt3 = "We are the so-called \tVikings from the north."
+print(txt3) #This is a string with a tab
+
+txt4 = "We are the so-called \bVikings from the north."
+print(txt4) #This is a string with a backspace
+
+txt5 = "We are the so-called \rVikings from the north."
+print(txt5) #This is a string with a carriage return
