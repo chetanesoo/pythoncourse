6ccd792bbebfd7d66247e678332fef1d84bb181a
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-16 20:32:10 +0400

    jointuples.py

---
+++ jointuples.py
@@ -0,0 +1,12 @@
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2 #join the two tuples
+print(tuple3)
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2 #repeat the tuple twice
+print(mytuple)
+
+fruits1 = ("apple", "banana", "cherry")
+mytuple1 = fruits1 * 3 #repeat the tuple three times
+print(mytuple1)
