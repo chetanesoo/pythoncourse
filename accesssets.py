1c8819c4a8a564c0b436b9b5aa04214433796ad6
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-17 20:40:37 +0400

    accesssets.py

---
+++ accesssets.py
@@ -0,0 +1,11 @@
+#This section is for accessing elements in a set
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+    print(x) #The for loop is used to iterate through the set
+    
+thisset1 = {"apple", "banana", "cherry"}
+print("banana" in thisset1) #Check if "banana" is in the set
+
+thisset2 = {"apple", "banana", "cherry"}
+print("banana" not in thisset2) #Check if "banana" is not in the set
