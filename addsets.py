9b3ac273dfc1d1f3fbbfa4f1d1eb4f81b31a5999
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-27 15:27:09 +0400

    addsets.py

---
+++ addsets.py
@@ -0,0 +1,13 @@
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange") #adds an element to the set
+print(thisset)
+
+thisset1 = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset1.update(tropical) #adds the elements of the tropical set to the thisset1 set
+print(thisset1)
+
+thisset2 = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset2.update(mylist) #adds the elements of the mylist to the thisset2 set
+print(thisset2)
