090cdce6cb51078f2a958bb403cd26564ba7fe05
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-07-27 15:40:45 +0400

    loopsets.py

---
+++ loopsets.py
@@ -0,0 +1,6 @@
+thisset = {"apple", "banana", "cherry"}
+for x in thisset: #loops through the set and prints each element
+    print(x)
+
+thisset1 = {"apple", "banana", "cherry"}
+print("Length of set:", len(thisset1))
