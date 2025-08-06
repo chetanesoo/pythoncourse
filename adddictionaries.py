157015804cb9da8f8f11d70d2719423efcba0d72
Chetane Sookdeb <chetanesookdeb@Chetanes-MacBook-Air.local>
2025-08-06 21:35:34 +0400

    adddictionaries.py

---
+++ adddictionaries.py
@@ -0,0 +1,15 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red" #add a new key-value pair
+print(thisdict) #print the whole dictionary
+
+thisdict1 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict1.update({"color": "red"}) #add a new key-value pair
+print(thisdict1) #print the whole dictionary
